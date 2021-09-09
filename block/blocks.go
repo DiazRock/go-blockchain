dcc012eff0317fcfdf0e3dcddfdffb467cc0a29c
DiazRock <DiazRock>
2021-09-09 18:08:34 -0400

    Changed the imported

--- block/blocks.go
+++ block/blocks.go
@@ -3,6 +3,8 @@ package block
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"log"
 	"strconv"
 	"time"
 )
@@ -31,4 +33,28 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 	block.Hash = hash[:]
 	block.Nonce = nonce
 	return block
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
