6b60041c82eec063676f3aef8800568b1f49fadb
DiazRock <DiazRock>
2021-09-13 17:10:30 -0400

    Just cli

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	blockchain "github.com/DiazRock/go-blockchain/blockchain_imp"
 	cli "github.com/DiazRock/go-blockchain/cli_imp"
 )
 
 func main() {
-	Bc := blockchain.NewBlockchain()
-	defer Bc.Db.Close()
-	cli_instance := cli.CLI{Bc}
-	cli_instance.Run()
+	cli := cli.CLI{}
+	cli.Run()
 }
