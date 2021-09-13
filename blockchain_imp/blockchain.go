da2844c170a1cfc6d3980f2b12fe98900172baff
DiazRock <DiazRock>
2021-09-13 17:09:59 -0400

    Gogogo

--- blockchain_imp/blockchain.go
+++ blockchain_imp/blockchain.go
@@ -1,15 +1,20 @@
 package blockchain_imp
 
 import (
+	"encoding/hex"
+	"fmt"
 	"log"
+	"os"
 
 	blocks "github.com/DiazRock/go-blockchain/block"
+	transactions_imp "github.com/DiazRock/go-blockchain/transactions_imp"
 	"github.com/boltdb/bolt"
 )
 
 const (
-	dbFile       = "blockchain.go"
-	blocksBucket = "blocks"
+	dbFile              = "blockchain.go"
+	blocksBucket        = "blocks"
+	genesisCoinbaseData = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
 )
 
 type Blockchain struct {
@@ -17,7 +22,8 @@ type Blockchain struct {
 	Db  *bolt.DB
 }
 
-func (bc *Blockchain) AddBlock(data string) {
+// MineBlock mines a new block with the provided transactions
+func (bc *Blockchain) MineBlock(transactions []*transactions_imp.Transaction) {
 	var lastHash []byte
 
 	err := bc.Db.View(func(tx *bolt.Tx) error {
@@ -28,34 +34,35 @@ func (bc *Blockchain) AddBlock(data string) {
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
-	newBlock := blocks.NewBlock(data, lastHash)
+	newBlock := blocks.NewBlock(transactions, lastHash)
 
 	err = bc.Db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
 		err := b.Put(newBlock.Hash, newBlock.Serialize())
-
 		if err != nil {
-			log.Fatal(err)
+			log.Panic(err)
 		}
 
 		err = b.Put([]byte("l"), newBlock.Hash)
-
 		if err != nil {
-			log.Fatal(err)
+			log.Panic(err)
 		}
+
 		bc.Tip = newBlock.Hash
 
 		return nil
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
-func NewBlockchain() *Blockchain {
+func NewBlockchain(address string) *Blockchain {
+	if !dbExists() {
+		fmt.Println("No existing blockchain found. Create one first.")
+		os.Exit(1)
+	}
+
 	var tip []byte
 	db, err := bolt.Open(dbFile, 0600, nil)
 	if err != nil {
@@ -64,32 +71,188 @@ func NewBlockchain() *Blockchain {
 
 	err = db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
+		tip = b.Get([]byte("l"))
 
-		if b == nil {
-			genesis := blocks.NewGenesisBlock()
-			b, err := tx.CreateBucket([]byte(blocksBucket))
-			if err != nil {
-				log.Panic(err)
-			}
-			err = b.Put(genesis.Hash, genesis.Serialize())
-			if err != nil {
-				log.Fatal(err)
-			}
-			err = b.Put([]byte("l"), genesis.Hash)
-			if err != nil {
-				log.Fatal(err)
-			}
-			tip = genesis.Hash
-		} else {
-			tip = b.Get([]byte("l"))
-		}
 		return nil
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
+
 	bc := Blockchain{tip, db}
 
 	return &bc
+}
+
+func dbExists() bool {
+	if _, err := os.Stat(dbFile); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+// CreateBlockchain creates a new blockchain DB
+func CreateBlockchain(address string) *Blockchain {
+	if dbExists() {
+		fmt.Println("Blockchain already exists.")
+		os.Exit(1)
+	}
+
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		cbtx := transactions_imp.NewCoinbaseTX(address, genesisCoinbaseData)
+		genesis := blocks.NewGenesisBlock(cbtx)
+
+		b, err := tx.CreateBucket([]byte(blocksBucket))
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put(genesis.Hash, genesis.Serialize())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = b.Put([]byte("l"), genesis.Hash)
+		if err != nil {
+			log.Panic(err)
+		}
+		tip = genesis.Hash
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bc := Blockchain{tip, db}
+
+	return &bc
+}
+
+func (bc *Blockchain) FindUnspentTransactions(address string) []transactions_imp.Transaction {
+	var unspentTxs []transactions_imp.Transaction
+	spentTXOs := make(map[string][]int)
+	bci := bc.Iterator()
+
+	for {
+		block := bci.Next()
+
+		for _, tx := range block.Transactions {
+			txID := hex.EncodeToString(tx.ID)
+
+		Outputs:
+			for outIdx, out := range tx.Vout {
+				// Was the output spent?
+				if spentTXOs[txID] != nil {
+					for _, spentOut := range spentTXOs[txID] {
+						if spentOut == outIdx {
+							continue Outputs
+						}
+					}
+				}
+
+				if out.CanBeUnlockedWith(address) {
+					unspentTxs = append(unspentTxs, *tx)
+				}
+			}
+			if tx.IsCoinbase() == false {
+				for _, in := range tx.Vin {
+					if in.CanUnlockOutputWith(address) {
+						inTxID := hex.EncodeToString(in.Txid)
+						spentTXOs[inTxID] = append(spentTXOs[inTxID], in.Vout)
+					}
+				}
+			}
+
+		}
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
+
+	return unspentTxs
+}
+
+func (bc *Blockchain) FindUTXO(address string) []transactions_imp.TXOutput {
+	var UTXOs []transactions_imp.TXOutput
+	unspentTransactions := bc.FindUnspentTransactions(address)
+
+	for _, tx := range unspentTransactions {
+		for _, out := range tx.Vout {
+			if out.CanBeUnlockedWith(address) {
+				UTXOs = append(UTXOs, out)
+			}
+		}
+	}
+
+	return UTXOs
+}
+
+func NewUTXOTransaction(from, to string, amount int, bc *Blockchain) *transactions_imp.Transaction {
+	var inputs []transactions_imp.TXInput
+	var outputs []transactions_imp.TXOutput
+
+	acc, validOutputs := bc.FindSpendableOutputs(from, amount)
+
+	if acc < amount {
+		log.Panic("Error: Not enough funds")
+	}
+
+	// Build a list of inputs
+
+	for txid, outs := range validOutputs {
+		txID, err := hex.DecodeString(txid)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, out := range outs {
+			input := transactions_imp.TXInput{txID, out, from}
+			inputs = append(inputs, input)
+		}
+	}
+
+	// Build a list of outputs
+	outputs = append(outputs, transactions_imp.TXOutput{amount, to})
+
+	if acc > amount {
+		outputs = append(outputs, transactions_imp.TXOutput{acc - amount, from}) // a change
+	}
+
+	tx := transactions_imp.Transaction{nil, inputs, outputs}
+	tx.SetID()
+
+	return &tx
+}
+
+func (bc *Blockchain) FindSpendableOutputs(address string, amount int) (int, map[string][]int) {
+	unspentOutputs := make(map[string][]int)
+	unspentTXs := bc.FindUnspentTransactions(address)
+	accumulated := 0
+
+Work:
+	for _, tx := range unspentTXs {
+		txID := hex.EncodeToString(tx.ID)
+
+		for outIdx, out := range tx.Vout {
+			if out.CanBeUnlockedWith(address) && accumulated < amount {
+				accumulated += out.Value
+				unspentOutputs[txID] = append(unspentOutputs[txID], outIdx)
+
+				if accumulated >= amount {
+					break Work
+				}
+			}
+		}
+	}
+
+	return accumulated, unspentOutputs
 }
